85fca68b8e673791cd7f0d76821a67740d078818
RodrigoVitor <rodrigovitorsc2001@gmail.com>
2022-04-19 22:56:38 -0300

    Adding footer component

---
+++ components/layout/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="Main">
+        <div class="container">
+            <b-row>
+                <b-col>
+                    <p>Receber uma recompensa após uma tarefa, é uma 
+                        das formas para nos motivar a continuar estudando.</p>
+                </b-col>
+                <b-col>
+                    <img src="/img/logo-mobile.png" alt="logo">
+                </b-col>
+            </b-row>
+            <b-row>
+                <b-col>
+                    <p class="text-center">by <a href="https://www.github.com/RodrigoVitor" target="_blank">Rodrigo Vitor</a> - 2022</p>
+                </b-col>
+            </b-row>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+#Main {
+    /* margin-top:25px; */
+    background-color: #FBFF1A;
+}
+</style>
