c5df076b1a8d5692a314e537e3edb9746b4219cb
RodrigoVitor <rodrigovitorsc2001@gmail.com>
2022-04-21 11:32:47 -0300

    modifying footer

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -3,16 +3,13 @@
         <div class="container">
             <b-row>
                 <b-col>
-                    <p>Receber uma recompensa após uma tarefa, é uma 
+                    <p class="text-center mt-4">Receber uma recompensa após uma tarefa,  <br> é uma 
                         das formas para nos motivar a continuar estudando.</p>
-                </b-col>
-                <b-col>
-                    <img src="/img/logo-mobile.png" alt="logo">
                 </b-col>
             </b-row>
             <b-row>
                 <b-col>
-                    <p class="text-center">by <a href="https://www.github.com/RodrigoVitor" target="_blank">Rodrigo Vitor</a> - 2022</p>
+                    <p class="text-center"> &copy; <a href="https://www.github.com/RodrigoVitor" target="_blank">Rodrigo Vitor</a> - 2022</p>
                 </b-col>
             </b-row>
         </div>
@@ -21,7 +18,11 @@
 
 <style scoped>
 #Main {
-    /* margin-top:25px; */
-    background-color: #FBFF1A;
+    background-color: #013d8b;
+    color:#fff
+}
+#Main a {
+    color:#fff;
+    font-weight: bold;
 }
 </style>
